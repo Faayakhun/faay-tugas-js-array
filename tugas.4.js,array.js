5b377c50ec5eb877e7aca710d0e78b7b24a8aac1
Faayakhun <faayakhun@gmail.com>
2021-01-27 23:52:25 +0700

    revisi untuk deploy

--- tugas.4.js,array.js
+++ tugas.4.js,array.js
@@ -3,8 +3,8 @@ let array2 = ["Geography", "Spanish", "Programming"];
 
 for (var i = 0; i < array1.length; i++) {
     if (array1[i] == array2[i]) {
-        console.log(true)
+        document.write(true + " ") 
     } else {
-        console.log(false)
+        document.write(false + " ")
     }
 }
